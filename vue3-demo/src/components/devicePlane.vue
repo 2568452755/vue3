6099641c431f7f5b1ba9a142074263981c6101cb
tygorge <2568452755@qq.com>
2023-02-10 16:51:04 +0800

    设备面板数据重构

--- vue3-demo/src/components/devicePlane.vue
+++ vue3-demo/src/components/devicePlane.vue
@@ -1,157 +1,221 @@
 <template>
 	<div class="container">
-		<div class="left">
-			<div>
-				<img alt="" :src="left_logo(1)" />
-			</div>
-			<div class="left-bottom">
-				<div class="l-b-container">
-					<!--					<img alt="" src="./assets/1ha.png" />-->
-					<!--					<img alt="" src="./assets/1管理.png" />-->
-					<template v-for="item in left_list" :key="item">
-						<el-tooltip effect="dark" placement="top">
-							<template #content>
-								<template v-if="item.status == 1">
-									网口{{ item.real_name }}<br />
-									流入包统计(个/秒) {{ item.rx_pkt }}<br />
-									流出包统计(个/秒) {{ item.send_pkt }}<br />
-									流入包比特速率<br />
-									(Kbps){{ (item.rx_byte_total / 8).toFixed(2) }}<br />
-									流出包比特速率<br />
-									(Kbps){{ (item.send_byte_total / 8).toFixed(2) }}<br />
-								</template>
-								<template v-else>
-									网口{{ item.real_name }}<br />
-									状态 未使用
-								</template>
-							</template>
-							<img :src="left_img(item)" alt="" />
-						</el-tooltip>
-					</template>
-				</div>
-			</div>
-		</div>
-		<div class="right">
-			<div class="r-con" v-for="slot in right_list" :key="slot">
-				<img :src="slot_img(slot)" class="slot" alt=" " v-if="slot && slot.grid_list.some((v) => v !== null)" />
-				<div class="grid-con">
-					<template v-for="grid in slot?.grid_list">
-						<el-tooltip effect="dark" placement="top">
-							<template #content>
-								<template v-if="grid?.showall?.status == 1">
-									网口 {{ grid.real_name }}<br />
-									流入包统计(个/秒) {{ grid?.showall?.rx_pkt }}<br />
-									流出包统计(个/秒) {{ grid?.showall?.send_pkt }}<br />
-									流入包比特速率(Kbps) {{ grid?.showall?.rx_byte }}<br />
-									流出包比特速率(Kbps) {{ grid?.showall?.send_byte }}<br />
-								</template>
-								<template v-else>
-									网口 {{ grid.real_name }}<br />
-									状态 未使用
-								</template>
-							</template>
-							<img alt="" :src="grid_img(grid)" :style="grid ? '' : { visibility: 'hidden' }" />
-						</el-tooltip>
-					</template>
-				</div>
-			</div>
-		</div>
+		<!--		<div class="left">-->
+		<!--			<div>-->
+		<!--				<img alt="" :src="left_logo(1)" />-->
+		<!--			</div>-->
+		<!--			<div class="left-bottom">-->
+		<!--				<div class="l-b-container">-->
+		<!--					&lt;!&ndash;					<img alt="" src="./assets/1ha.png" />&ndash;&gt;-->
+		<!--					&lt;!&ndash;					<img alt="" src="./assets/1管理.png" />&ndash;&gt;-->
+		<!--					<template v-for="item in left_list" :key="item">-->
+		<!--						<el-tooltip effect="dark" placement="top">-->
+		<!--							<template #content>-->
+		<!--								<template v-if="item.status == 1">-->
+		<!--									网口{{ item.real_name }}<br />-->
+		<!--									流入包统计(个/秒) {{ item.rx_pkt }}<br />-->
+		<!--									流出包统计(个/秒) {{ item.send_pkt }}<br />-->
+		<!--									流入包比特速率<br />-->
+		<!--									(Kbps){{ (item.rx_byte_total / 8).toFixed(2) }}<br />-->
+		<!--									流出包比特速率<br />-->
+		<!--									(Kbps){{ (item.send_byte_total / 8).toFixed(2) }}<br />-->
+		<!--								</template>-->
+		<!--								<template v-else>-->
+		<!--									网口{{ item.real_name }}<br />-->
+		<!--									状态 未使用-->
+		<!--								</template>-->
+		<!--							</template>-->
+		<!--							<img :src="left_img(item)" alt="" />-->
+		<!--						</el-tooltip>-->
+		<!--					</template>-->
+		<!--				</div>-->
+		<!--			</div>-->
+		<!--		</div>-->
+		<!--		<div class="right">-->
+		<!--			<div class="r-con" v-for="slot in right_list" :key="slot">-->
+		<!--				<img :src="slot_img(slot)" class="slot" alt=" " v-if="slot && slot.grid_list.some((v) => v !== null)" />-->
+		<!--				<div class="grid-con">-->
+		<!--					<template v-for="grid in slot?.grid_list">-->
+		<!--						<el-tooltip effect="dark" placement="top">-->
+		<!--							<template #content>-->
+		<!--								<template v-if="grid?.showall?.status == 1">-->
+		<!--									网口 {{ grid.real_name }}<br />-->
+		<!--									流入包统计(个/秒) {{ grid?.showall?.rx_pkt }}<br />-->
+		<!--									流出包统计(个/秒) {{ grid?.showall?.send_pkt }}<br />-->
+		<!--									流入包比特速率(Kbps) {{ grid?.showall?.rx_byte }}<br />-->
+		<!--									流出包比特速率(Kbps) {{ grid?.showall?.send_byte }}<br />-->
+		<!--								</template>-->
+		<!--								<template v-else>-->
+		<!--									网口 {{ grid.real_name }}<br />-->
+		<!--									状态 未使用-->
+		<!--								</template>-->
+		<!--							</template>-->
+		<!--							<img alt="" :src="grid_img(grid)" :style="grid ? '' : { visibility: 'hidden' }" />-->
+		<!--						</el-tooltip>-->
+		<!--					</template>-->
+		<!--				</div>-->
+		<!--			</div>-->
+		<!--		</div>-->
 	</div>
 	<el-button type="primary">Primary</el-button>
 </template>
 <script setup>
 import { computed } from 'vue'
 import { data } from '../data.js'
-// import { $ref } from 'vue/macros'
-//一、面板logo
-let serial_no = $ref('012')
-let language = $ref('zh')
-let left_logo_list = {
-	wangyuzh: 'wangyu.png',
-	wangyuen: 'Leadsec.png',
-	qimingzh: 'qiming.png',
-	qimingen: 'Venustech.png',
+// // import { $ref } from 'vue/macros'
+// //一、面板logo
+// let serial_no = $ref('012')
+// let language = $ref('zh')
+// let left_logo_list = {
+// 	wangyuzh: 'wangyu.png',
+// 	wangyuen: 'Leadsec.png',
+// 	qimingzh: 'qiming.png',
+// 	qimingen: 'Venustech.png',
+// }
+// const left_logo = computed(() => {
+// 	return (item) => {
+// 		let imgName = left_logo_list[(serial_no == '221' ? 'wangyu' : 'qiming') + language]
+//
+// 		return new URL(`./assets/${imgName}`, import.meta.url).href
+// 	}
+// })
+// //二、左侧插口
+// let left_list = $ref(data.portList.showall_param.filter((v) => v.interface_type !== 0))
+// let left_img_list = {
+// 	//interface_type_1_status_1_lang_zh
+// 	it_1_s_1_la_zh: '1管理.png',
+// 	it_1_s_1_la_en: '1管理en.png',
+// 	it_1_s_0_la_zh: '0管理.png',
+// 	it_1_s_0_la_en: '0管理en.png',
+// 	it_2_s_1_la_zh: '1ha.png',
+// 	it_2_s_1_la_en: '1ha.png',
+// 	it_2_s_0_la_zh: '0ha.png',
+// 	it_2_s_0_la_en: '0ha.png',
+// }
+// let left_img = computed(() => {
+// 	return (item) => {
+// 		let imgName = left_img_list[`it_${item.interface_type}_s_${item.status}_la_${language}`]
+// 		return new URL(`./assets/${imgName}`, import.meta.url).href
+// 	}
+// })
+// //三、面板数据
+// let right_list = $ref([Array(4), Array(4)])
+// //面板排序
+// data.layoutList.slot_param.forEach((item, index) => {
+// 	item.grid_list = [Array(4), Array(4)]
+// 	right_list[item.row][item.col] = item
+// })
+// //二维转一维
+// right_list = right_list[0].concat(right_list[1])
+//
+// //接口数据
+// right_list.forEach((slot) => {
+// 	data.layoutList.grid_param.forEach((grid) => {
+// 		//参数放入接口
+// 		// data.portList.showall_param.forEach((showall) => {
+// 		// 	if (grid.real_name === showall.real_name) {
+// 		// 		grid.showall = showall
+// 		// 	}
+// 		// })
+// 		grid.showall = data.portList.showall_param.filter((showall) => {
+// 			return grid.real_name === showall.real_name
+// 		})[0]
+// 		//接口放入卡槽
+// 		if (slot.slot_id === grid.slot_id) {
+// 			slot.grid_list[grid.row][grid.col] = grid
+// 		}
+// 	})
+// 	//二维转一维
+// 	slot.grid_list = slot.grid_list[0].concat(slot.grid_list[1])
+// })
+// let slot_img_list = ['slot0.png', 'slot1.png', 'slot2.png', 'slot3.png', 'slot4.png', 'slot5.png', 'slot6.png', 'slot7.png']
+// let slot_img = computed(() => {
+// 	return (slot) => {
+// 		let imgName = slot_img_list[slot?.slot_id]
+// 		return new URL(`./assets/${imgName}`, import.meta.url).href
+// 	}
+// })
+// let grid_img_list = {
+// 	status_0_media_2: '0圆.png',
+// 	status_0_media_0_xge: '0圆.png',
+// 	status_0_media_1: '0方.png',
+// 	status_0_media_0_gev: '0方.png',
+// 	status_1_media_2: '1圆.png',
+// 	status_1_media_0_xge: '1圆.png',
+// 	status_1_media_1: '1方.png',
+// 	status_1_media_0_gev: '1方.png',
+// }
+// let grid_img = computed(() => {
+// 	return (grid) => {
+// 		let ge = '_' + grid?.media_type == 0 ? grid?.real_name.slice(0, 3) : ''
+// 		let imgName = grid_img_list[`status_${grid?.showall?.status}_media_${grid?.media_type}${ge}`]
+// 		return new URL(`./assets/${imgName}`, import.meta.url).href
+// 	}
+// })
+//--------------数据结构转化
+let device = $ref([])
+const { row_total, col_total } = data.layoutList
+//面板结构建立
+for (let i = 0; i < row_total; i++) {
+	device.push([])
+	for (let j = 0; j < col_total; j++) {
+		device[i].push([])
+	}
 }
-const left_logo = computed(() => {
-	return (item) => {
-		let imgName = left_logo_list[(serial_no == '221' ? 'wangyu' : 'qiming') + language]
+let showall_param = data.portList.showall_param
+const { slot_param, grid_param } = data.layoutList
+//接口数据和参数合并
+let grid_list = $ref([])
+grid_list = grid_param.map((grid) => {
+	grid.params = showall_param.filter((show) => {
+		return grid.real_name === show.real_name
+	})[0]
+	return grid
+})
 
-		return new URL(`./assets/${imgName}`, import.meta.url).href
-	}
-})
-//二、左侧插口
-let left_list = $ref(data.portList.showall_param.filter((v) => v.interface_type !== 0))
-let left_img_list = {
-	//interface_type_1_status_1_lang_zh
-	it_1_s_1_la_zh: '1管理.png',
-	it_1_s_1_la_en: '1管理en.png',
-	it_1_s_0_la_zh: '0管理.png',
-	it_1_s_0_la_en: '0管理en.png',
-	it_2_s_1_la_zh: '1ha.png',
-	it_2_s_1_la_en: '1ha.png',
-	it_2_s_0_la_zh: '0ha.png',
-	it_2_s_0_la_en: '0ha.png',
-}
-let left_img = computed(() => {
-	return (item) => {
-		let imgName = left_img_list[`it_${item.interface_type}_s_${item.status}_la_${language}`]
-		return new URL(`./assets/${imgName}`, import.meta.url).href
-	}
-})
-//三、面板数据
-let right_list = $ref([Array(4), Array(4)])
-//面板排序
-data.layoutList.slot_param.forEach((item, index) => {
-	item.grid_list = [Array(4), Array(4)]
-	right_list[item.row][item.col] = item
-})
-//二维转一维
-right_list = right_list[0].concat(right_list[1])
+for (let i = 0; i < device.length; i++) {
+	let len = device[i].length
+	for (let j = 0; j < len; j++) {
+		slot_param.forEach((slot) => {
+			//从接口数组中过滤出当前面板的接口
+			let slot_grid_list = grid_list.filter((grid) => {
+				return grid.slot_id === slot.slot_id
+			})
 
-//接口数据
-right_list.forEach((slot) => {
-	data.layoutList.grid_param.forEach((grid) => {
-		//参数放入接口
-		// data.portList.showall_param.forEach((showall) => {
-		// 	if (grid.real_name === showall.real_name) {
-		// 		grid.showall = showall
-		// 	}
-		// })
-		grid.showall = data.portList.showall_param.filter((showall) => {
-			return grid.real_name === showall.real_name
-		})[0]
-		//接口放入卡槽
-		if (slot.slot_id === grid.slot_id) {
-			slot.grid_list[grid.row][grid.col] = grid
-		}
-	})
-	//二维转一维
-	slot.grid_list = slot.grid_list[0].concat(slot.grid_list[1])
-})
-let slot_img_list = ['slot0.png', 'slot1.png', 'slot2.png', 'slot3.png', 'slot4.png', 'slot5.png', 'slot6.png', 'slot7.png']
-let slot_img = computed(() => {
-	return (slot) => {
-		let imgName = slot_img_list[slot?.slot_id]
-		return new URL(`./assets/${imgName}`, import.meta.url).href
+			//定义2*4面板结构
+			let slot_List = []
+			for (let i = 0; i < 2; i++) {
+				slot_List.push([])
+				for (let j = 0; j < 4; j++) {
+					slot_List[i].push([])
+				}
+			}
+			let row2 = slot_grid_list.some((v) => {
+				return v.row === 2
+			})
+			if (row2) {
+				slot_List.push([[], []])
+			}
+			//把接口放入面板
+			slot_grid_list.forEach((grid) => {
+				//c264设备
+				slot_List[grid.row][grid.col] = grid
+			})
+			//c264设备
+			if (row2) {
+				slot_List[0].unshift([], [])
+				slot_List[1].unshift([], [])
+				slot_List[1][0] = slot_List[2][0]
+				slot_List[1][1] = slot_List[2][1]
+				slot_List.pop()
+			}
+			slot.slot_List = slot_List
+			//将slot放入面板
+			device[slot.row][slot.col] = slot
+		})
 	}
-})
-let grid_img_list = {
-	status_0_media_2: '0圆.png',
-	status_0_media_0_xge: '0圆.png',
-	status_0_media_1: '0方.png',
-	status_0_media_0_gev: '0方.png',
-	status_1_media_2: '1圆.png',
-	status_1_media_0_xge: '1圆.png',
-	status_1_media_1: '1方.png',
-	status_1_media_0_gev: '1方.png',
 }
-let grid_img = computed(() => {
-	return (grid) => {
-		let ge = '_' + grid?.media_type == 0 ? grid?.real_name.slice(0, 3) : ''
-		let imgName = grid_img_list[`status_${grid?.showall?.status}_media_${grid?.media_type}${ge}`]
-		return new URL(`./assets/${imgName}`, import.meta.url).href
-	}
-})
+console.log(device)
 </script>
 <style lang="scss" scoped>
 .container {
